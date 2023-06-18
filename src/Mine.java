342169776ff8e32f4fad90bed3ec2847cdb7f091
Ali Yılmaz <123991935+aliylmztr@users.noreply.github.com>
2023-06-18 17:54:17 +0300

    Update Mine.java

--- src/Mine.java
+++ src/Mine.java
@@ -1,5 +1,5 @@
 public class Mine extends BattleLoc {
-    public Mine(Player player){
+    public Mine(Player player) {
         super(player,"Maden ⛏", new Snake(player), "Silah ⚔ , Zırh 🛡️ ya da Para 💵");
     }
 }
