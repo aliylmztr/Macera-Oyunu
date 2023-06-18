39cab525d6122cb03ce95f939e4a7e691c4c8a87
Ali Yılmaz <123991935+aliylmztr@users.noreply.github.com>
2023-06-18 17:56:13 +0300

    Update Vampire.java

--- src/Vampire.java
+++ src/Vampire.java
@@ -1,5 +1,5 @@
 public class Vampire extends Obstacle {
     public Vampire(Player player) {
-        super(player,"Vampir🦇", 2, 4, 14, 7, 1);
+        super(player, "Vampir🦇", 2, 4, 14, 7, 1);
     }
 }
